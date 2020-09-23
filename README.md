886fa27f840ed792ab30ff142052360734c8e1a0
yonghao <yyhao1@gmail.com>
2020-09-23 11:31:08 -0400

    initial RBS

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# terra-rbs
-Terra Resource Buffering Service
+# Terra Resource Buffering Server
+Cloud resource buffering server for Terra. 
+
+# Deployment
+## Run Locally
+Set executable permissions:
+```
+chmod +x gradlew
+```
+
+To spin up the local postgres, run:
+```
+local-dev/run_postgres.sh start
+```
+Start local server
+```
+local-dev/run_local.sh
+```
+## TODO: Add more
---
+++ src/main/java/bio/terra/TEMPLATE/app/configuration/ApiResourceConfig.java
@@ -0,0 +1,16 @@
+package bio.terra.TEMPLATE.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiResourceConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/api/swagger-webjar/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.0/");
+    registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
+  }
+}
