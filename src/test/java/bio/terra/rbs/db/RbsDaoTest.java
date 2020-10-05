ee1ea2fa1b7821bbe9b14c33276f426931b36e26
yonghao <yyhao1@gmail.com>
2020-10-05 14:12:33 -0400

    fix test

--- src/test/java/bio/terra/rbs/db/RbsDaoTest.java
+++ src/test/java/bio/terra/rbs/db/RbsDaoTest.java
@@ -15,8 +15,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RbsDaoTest extends BaseUnitTest {
   @Autowired RbsJdbcConfiguration jdbcConfiguration;
   @Autowired RbsDao rbsDao;
