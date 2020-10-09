f7281dd2d76f13b0cdb9196f987166fb4c284ffe
yonghao <yyhao1@gmail.com>
2020-10-09 17:51:27 -0400

    initial delete/create empty resource flight

--- src/main/java/bio/terra/rbs/app/configuration/BeanNames.java
+++ src/main/java/bio/terra/rbs/app/configuration/BeanNames.java
@@ -6,5 +6,4 @@ public class BeanNames {
 
   public static final String JDBC_TEMPLATE = "jdbcTemplate";
   public static final String OBJECT_MAPPER = "objectMapper";
-  public static final String FLIGHT_FACTORY_IMPL = "FlightFactoryImpl";
 }
