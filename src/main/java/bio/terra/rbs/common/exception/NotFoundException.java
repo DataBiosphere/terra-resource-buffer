8ef561b7bc3362e17b8979344b127b3804e9dc77
yonghao <yyhao1@gmail.com>
2020-10-21 10:22:29 -0400

    extend ErrorReportException

--- src/main/java/bio/terra/rbs/common/exception/NotFoundException.java
+++ src/main/java/bio/terra/rbs/common/exception/NotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 /** This exception maps to HttpStatus.NOT_FOUND in the GlobalExceptionHandler. */
 public class NotFoundException extends ErrorReportException {
-  private static final HttpStatus thisStatus = HttpStatus.BAD_REQUEST;
+  private static final HttpStatus thisStatus = HttpStatus.NOT_FOUND;
 
   public NotFoundException(String message) {
     super(message, null, thisStatus);
