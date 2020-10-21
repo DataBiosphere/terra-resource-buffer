8f04e7631342b8d3e65a605e699482b6afe084f7
yonghao <yyhao1@gmail.com>
2020-10-20 21:53:01 -0400

    not found exception

--- src/main/java/bio/terra/rbs/common/exception/NotFoundException.java
+++ src/main/java/bio/terra/rbs/common/exception/NotFoundException.java
@@ -2,11 +2,11 @@ package bio.terra.rbs.common.exception;
 
 /** This exception maps to HttpStatus.NOT_FOUND in the GlobalExceptionHandler. */
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
+  public NotFoundException(String message) {
+    super(message);
+  }
 
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public NotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
