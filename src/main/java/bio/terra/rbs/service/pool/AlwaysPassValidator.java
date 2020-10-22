94ab12b58bcea2a872b742c4c87c5425755257bc
yonghao <yyhao1@gmail.com>
2020-10-22 12:49:11 -0400

    fix test

--- src/main/java/bio/terra/rbs/service/pool/AlwaysPassValidator.java
+++ src/main/java/bio/terra/rbs/service/pool/AlwaysPassValidator.java
@@ -2,7 +2,7 @@ package bio.terra.rbs.service.pool;
 
 import bio.terra.rbs.generated.model.ResourceConfig;
 
-/** The default validator which does not no restrictions. */
+/** The validator which does not no restrictions. */
 public class AlwaysPassValidator implements ResourceConfigValidator {
   @Override
   public void validate(ResourceConfig config) {
