e2d03afbf32729453880674aed94c4fd053590d3
yonghao <yyhao1@gmail.com>
2020-11-04 21:45:31 -0500

    one multuple step

--- src/main/java/bio/terra/rbs/service/resource/flight/StepUtils.java
+++ src/main/java/bio/terra/rbs/service/resource/flight/StepUtils.java
@@ -3,6 +3,7 @@ package bio.terra.rbs.service.resource.flight;
 import static bio.terra.rbs.service.resource.FlightMapKeys.RESOURCE_READY;
 
 import bio.terra.rbs.common.ResourceId;
+import bio.terra.rbs.common.ResourceState;
 import bio.terra.rbs.db.RbsDao;
 import bio.terra.rbs.generated.model.CloudResourceUid;
 import bio.terra.rbs.service.resource.FlightMapKeys;
@@ -15,9 +16,13 @@ public class StepUtils {
   /** Update resource state to READY and update working map's RESOURCE_READY boolean value. */
   public static void markResourceReady(RbsDao rbsDao, FlightContext flightContext) {
     FlightMap workingMap = flightContext.getWorkingMap();
-    rbsDao.updateResourceAsReady(
-        ResourceId.retrieve(flightContext.getWorkingMap()),
-        workingMap.get(FlightMapKeys.CLOUD_RESOURCE_UID, CloudResourceUid.class));
+    ResourceId resourceId = ResourceId.retrieve(workingMap);
+    if (rbsDao.retrieveResource(resourceId).get().state().equals(ResourceState.CREATING)) {
+      // Just in case the step retries even after DB is updated to READY.
+      rbsDao.updateResourceAsReady(
+          ResourceId.retrieve(flightContext.getWorkingMap()),
+          workingMap.get(FlightMapKeys.CLOUD_RESOURCE_UID, CloudResourceUid.class));
+    }
     workingMap.put(RESOURCE_READY, true);
   }
   /** Check resource is already marked as READY. This can prevent a READY resource got rollback. */
