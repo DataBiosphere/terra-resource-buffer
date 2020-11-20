3d72ca8d28d5cbfd61c0ea52d4fa021a6d976e43
yonghao <yyhao1@gmail.com>
2020-11-20 09:59:09 -0500

    spotlessapply

--- local-dev/run_local.sh
+++ local-dev/run_local.sh
@@ -9,16 +9,9 @@ export BUFFER_STAIRWAY_DB_PASSWORD=dbpwd_stairway
 export BUFFER_STAIRWAY_DB_FORCE_CLEAN_START=true
 export BUFFER_STAIRWAY_DATABASE_NAME=testdb_stairway
 export GOOGLE_APPLICATION_CREDENTIALS="$(dirname $0)"/../src/test/resources/rendered/sa-account.json
-<<<<<<< HEAD
 export BUFFER_CRL_TESTING_MODE=true
 export BUFFER_CRL_JANITOR_CLIENT_CREDENTIAL_FILE_PATH="$(dirname $0)"/../src/test/resources/rendered/janitor-client-sa-account.json
 export BUFFER_CRL_JANITOR_TRACK_RESOURCE_PROJECT_ID=terra-kernel-k8s
 export BUFFER_CRL_JANITOR_TRACK_RESOURCE_TOPIC_ID=crljanitor-tools-pubsub-topic
-=======
-export RBS_CRL_TESTING_MODE=true
-export RBS_CRL_JANITOR_CLIENT_CREDENTIAL_FILE_PATH="$(dirname $0)"/../src/test/resources/rendered/janitor-client-sa-account.json
-export RBS_CRL_JANITOR_TRACK_RESOURCE_PROJECT_ID=terra-kernel-k8s
-export RBS_CRL_JANITOR_TRACK_RESOURCE_TOPIC_ID=crljanitor-tools-pubsub-topic
-export RBS_POOL_CONFIG_PATH=config/toolsalpha
->>>>>>> master
+
 ./gradlew bootRun
