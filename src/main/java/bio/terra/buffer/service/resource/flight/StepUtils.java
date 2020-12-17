12afba8a634b9eb1109b668ee7b30d4e4e9fa314
Yonghao Yu <yyhao1@gmail.com>
2020-12-17 16:55:22 -0500

    [PF-287] Add retry rules for all steps (#84)
    * spotbugs
    
    * spotbugs
    
    * try upload report as artifact
    
    * try upload report as artifact
    
    * test
    
    * do not generate badge
    
    * do not generate badge
    
    * fixx pth
    
    * fix typo
    
    * depends on test
    
    * depends on test
    
    * shouldRunafter
    
    * shouldRunafter
    
    * test
    
    * tmp
    
    * fix flaky test
    
    * tmp
    
    * add read me
    
    * reorder things
    
    * try jacoco
    
    * test
    
    * test
    
    * I have coverage report now
    
    * finalize
    
    * finalize
    
    * config
    
    * add more
    
    * revert
    
    * two rules
    
    * merge master

--- src/main/java/bio/terra/buffer/service/resource/flight/StepUtils.java
+++ src/main/java/bio/terra/buffer/service/resource/flight/StepUtils.java
@@ -8,9 +8,23 @@ import bio.terra.buffer.generated.model.CloudResourceUid;
 import bio.terra.buffer.service.resource.FlightMapKeys;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
+import bio.terra.stairway.RetryRuleFixedInterval;
 
 /** Utilities used in Stairway steps. */
 public class StepUtils {
+  /**
+   * The stairway retry rule for GCP API calls. Use longer wait time because cloud API quota,
+   * outrage, and other unknown issues.
+   */
+  public static final RetryRuleFixedInterval CLOUD_API_DEFAULT_RETRY =
+      new RetryRuleFixedInterval(/* intervalSeconds =*/ 90, /* maxCount =*/ 5);
+
+  /**
+   * The stairway retry rule for Buffer service internal operations. Use shorter wait time because
+   * they all internal operations, e.g. DB write/read. And we are able to retry right away.
+   */
+  public static final RetryRuleFixedInterval INTERNAL_DEFAULT_RETRY =
+      new RetryRuleFixedInterval(/* intervalSeconds =*/ 5, /* maxCount =*/ 5);
 
   /** Update resource state to READY and update working map's RESOURCE_READY boolean value. */
   public static void markResourceReady(BufferDao bufferDao, FlightContext flightContext) {
