845ee56a9096d81be3bb376ce8a6f4532820fc79
yonghao <yyhao1@gmail.com>
2020-12-21 22:11:24 -0500

    tmp

--- buffer-clienttests/src/main/java/scripts/utils/BufferServiceUtils.java
+++ buffer-clienttests/src/main/java/scripts/utils/BufferServiceUtils.java
@@ -29,7 +29,7 @@ public class BufferServiceUtils {
    * The pool id to get projects from. Pool config can be found at src/resources/config/perf folder
    * under Buffer Service repo.
    */
-  public static final String POOL_ID = "resource_perf_v1";
+  public static final String POOL_ID = "resource_toolsalpha_v1";
 
   /** How ofter to poll from buffer service. */
   public static final Duration POLLING_INTERVAL = Duration.ofMinutes(1);
