0525678cb93e199457bcbd3372026842538b4212
yonghao <yyhao1@gmail.com>
2021-01-31 19:40:09 -0500

    library

--- src/main/java/bio/terra/buffer/service/migrate/exception/MigrateException.java
+++ src/main/java/bio/terra/buffer/service/migrate/exception/MigrateException.java
@@ -1,6 +1,5 @@
 package bio.terra.buffer.service.migrate.exception;
 
-import bio.terra.buffer.common.exception.InternalServerErrorException;
 import bio.terra.common.exception.InternalServerErrorException;
 
 public class MigrateException extends InternalServerErrorException {
