a8848daae599865491e92506752dd1638500215f
Yonghao Yu <yyhao1@gmail.com>
2021-02-19 10:15:37 -0500

    [PF-161]: Multiple instance stairway support  (#114)
    * test
    
    * test
    
    * commit
    
    * tmp
    
    * test
    
    * commit
    
    * project id
    
    * fix name
    
    * remove cluster name
    
    * address comment
    
    * atomic
    
    * atomic
    
    * test
    
    * tmp

--- src/main/java/bio/terra/buffer/service/stairway/StairwayComponent.java
+++ src/main/java/bio/terra/buffer/service/stairway/StairwayComponent.java
@@ -1,13 +1,20 @@
 package bio.terra.buffer.service.stairway;
 
+import static com.google.cloud.ServiceOptions.getDefaultProjectId;
+
 import bio.terra.buffer.app.configuration.StairwayConfiguration;
 import bio.terra.buffer.app.configuration.StairwayJdbcConfiguration;
+import bio.terra.buffer.service.kubernetes.KubernetesComponent;
+import bio.terra.common.kubernetes.KubeService;
 import bio.terra.common.stairway.TracingHook;
 import bio.terra.stairway.Stairway;
 import bio.terra.stairway.exception.StairwayException;
 import bio.terra.stairway.exception.StairwayExecutionException;
-import com.google.common.collect.ImmutableList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +29,7 @@ public class StairwayComponent {
   private final StairwayConfiguration stairwayConfiguration;
   private final StairwayJdbcConfiguration stairwayJdbcConfiguration;
   private final Stairway stairway;
+  private final KubeService kubeService;
 
   public enum Status {
     INITIALIZING,
@@ -30,29 +38,34 @@ public class StairwayComponent {
     SHUTDOWN,
   }
 
-  private Status status = Status.INITIALIZING;
+  private final AtomicReference<Status> status = new AtomicReference<>(Status.INITIALIZING);
 
   @Autowired
   public StairwayComponent(
       ApplicationContext applicationContext,
       StairwayConfiguration stairwayConfiguration,
-      StairwayJdbcConfiguration stairwayJdbcConfiguration) {
+      StairwayJdbcConfiguration stairwayJdbcConfiguration,
+      KubernetesComponent kubernetesComponent) {
     this.stairwayConfiguration = stairwayConfiguration;
     this.stairwayJdbcConfiguration = stairwayJdbcConfiguration;
-
+    this.kubeService = kubernetesComponent.get();
+    String stairwayClusterName = kubeService.getNamespace() + "buffer--stairwaycluster";
     logger.info(
         "Creating Stairway: name: [{}]  cluster name: [{}]",
-        stairwayConfiguration.getName(),
-        stairwayConfiguration.getClusterName());
-    // TODO(PF-161): Configure the workqueue pubsub subscription and topic for multi-instance.
+        kubeService.getPodName(),
+        stairwayClusterName);
+
     // TODO(PF-314): Cleanup old flightlogs.
+    // TODO(PF-456):
     Stairway.Builder builder =
         Stairway.newBuilder()
             .maxParallelFlights(stairwayConfiguration.getMaxParallelFlights())
             .applicationContext(applicationContext)
             .keepFlightLog(true)
-            .stairwayName(stairwayConfiguration.getName())
-            .stairwayClusterName(stairwayConfiguration.getClusterName())
+            .stairwayName(kubeService.getPodName())
+            .stairwayClusterName(stairwayClusterName)
+            .workQueueProjectId(getDefaultProjectId())
+            .enableWorkQueue(kubernetesComponent.isInKubernetes())
             .stairwayHook(new TracingHook());
     try {
       stairway = builder.build();
@@ -62,25 +75,48 @@ public class StairwayComponent {
   }
 
   public void initialize() {
-    logger.warn("stairway username {}", stairwayJdbcConfiguration.getUsername());
+    logger.info("Initializing Stairway...");
+    logger.info("stairway username {}", stairwayJdbcConfiguration.getUsername());
     try {
       // TODO(PF-161): Determine if Stairway and buffer database migrations need to be coordinated.
-      stairway.initialize(
-          stairwayJdbcConfiguration.getDataSource(),
-          stairwayConfiguration.isForceCleanStart(),
-          stairwayConfiguration.isMigrateUpgrade());
-      // (PF-161): Get obsolete Stairway instances from k8s for multi-instance stairway.
-      stairway.recoverAndStart(ImmutableList.of(stairwayConfiguration.getName()));
+      List<String> recordedStairways =
+          stairway.initialize(
+              stairwayJdbcConfiguration.getDataSource(),
+              stairwayConfiguration.isForceCleanStart(),
+              stairwayConfiguration.isMigrateUpgrade());
+
+      kubeService.startPodListener(stairway);
+
+      // Lookup all of the stairway instances we know about
+      Set<String> existingStairways = kubeService.getPodSet();
+      List<String> obsoleteStairways = new LinkedList<>();
+
+      // Any instances that stairway knows about, but we cannot see are obsolete.
+      for (String recordedStairway : recordedStairways) {
+        if (!existingStairways.contains(recordedStairway)) {
+          obsoleteStairways.add(recordedStairway);
+        }
+      }
+
+      // Add our own pod name to the list of obsolete stairways. Sometimes Kubernetes will
+      // restart the container without redeploying the pod. In that case we must ask
+      // Stairway to recover the flights we were working on before being restarted.
+      obsoleteStairways.add(kubeService.getPodName());
+
+      logger.info(
+          "existingStairways: {}. obsoleteStairways: {}", existingStairways, obsoleteStairways);
+      // Recover and start stairway - step 3 of the stairway startup sequence
+      stairway.recoverAndStart(obsoleteStairways);
     } catch (StairwayException | InterruptedException e) {
-      status = Status.ERROR;
+      status.compareAndSet(Status.INITIALIZING, Status.ERROR);
       throw new RuntimeException("Error starting Stairway", e);
     }
-    status = Status.OK;
+    status.compareAndSet(Status.INITIALIZING, Status.OK);
   }
 
   /** Stop accepting jobs and shutdown stairway. Returns true if successful. */
   public boolean shutdown() throws InterruptedException {
-    status = Status.SHUTDOWN;
+    status.set(Status.SHUTDOWN);
     logger.info("Request Stairway shutdown");
     boolean shutdownSuccess =
         stairway.quietDown(
@@ -100,6 +136,6 @@ public class StairwayComponent {
   }
 
   public StairwayComponent.Status getStatus() {
-    return status;
+    return status.get();
   }
 }
