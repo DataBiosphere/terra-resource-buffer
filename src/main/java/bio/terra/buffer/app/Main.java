185b1cb047417aa06ce91a31cc58c7e628197f37
yonghao <yyhao1@gmail.com>
2021-02-23 11:21:33 -0500

    tmp

--- src/main/java/bio/terra/buffer/app/Main.java
+++ src/main/java/bio/terra/buffer/app/Main.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(
+    basePackages = {
+      // Scan for Liquibase migration components & configs
+      "bio.terra.common.migrate",
+      // Scan all service-specific packages beneath the current package
+      "bio.terra.buffer"
+    })
 @ComponentScan(basePackages = {"bio.terra.buffer", "bio.terra.common"})
 public class Main {
   public static void main(String[] args) {
