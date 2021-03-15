bcdf2f95c554d71428cf161df18269008ab3485e
Gregory Jordan <gjordan@verily.com>
2021-03-15 09:43:21 -0400

    Remove the root bio.terra.common import, and add bio.terra.common.logging

--- src/main/java/bio/terra/buffer/app/Main.java
+++ src/main/java/bio/terra/buffer/app/Main.java
@@ -8,14 +8,15 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(
     basePackages = {
-      // Scan for Liquibase migration components & configs
+      // Logging components & configs
+      "bio.terra.common.logging",
+      // Liquibase migration components & configs
       "bio.terra.common.migrate",
-      // Scan for tracing-related components & configs
+      // Tracing-related components & configs
       "bio.terra.common.tracing",
       // Scan all service-specific packages beneath the current package
       "bio.terra.buffer"
     })
-@ComponentScan(basePackages = {"bio.terra.buffer", "bio.terra.common"})
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
