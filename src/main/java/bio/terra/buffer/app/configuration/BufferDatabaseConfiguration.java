a7c7ed183bd3f3f6b34a62adf1c6ca197929e21e
yonghao <yyhao1@gmail.com>
2021-03-18 19:03:25 -0400

    fixing test

--- src/main/java/bio/terra/buffer/app/configuration/BufferDatabaseConfiguration.java
+++ src/main/java/bio/terra/buffer/app/configuration/BufferDatabaseConfiguration.java
@@ -9,9 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableConfigurationProperties(value = BufferDatabaseProperties.class)
+@EnableTransactionManagement
 public class BufferDatabaseConfiguration {
   private final BufferDatabaseProperties databaseProperties;
   // These properties control code in the StartupInitializer. We would not use these in production,
