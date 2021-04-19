5d6c4c553d2b507ace2a5707f7b4c18ad2c4a026
Aaron Kemp <80846577+kemp-verily@users.noreply.github.com>
2021-04-19 11:42:02 -0400

    [PF-228] Add support for two word project IDs (#144)
    This adds support for configuring GCP style adjective-noun project IDs
    using a pre-populated list of word pairs.
    
    The word list was generated using the `wn` tool from Princeton's Wordnet
    project.
    
    I started with a seed list of positive adjectives (happy, fast, etc) and
    used `wn` to generate synonyms.  For nouns, I used a seed list of
    categories (fruit, vegetable, jewel) and used `wn` to generate all
    words with an "is a kind of" relation.
    
    I filtered the words based on length, and then manually reviewed and
    filtered the resulting lists before generating the pairwise combinations.
    
    For posterity, another approach I tried and ultimately abandoned was to
    use Sentinet to perform automated review of the lists.  There were too
    many aberrations in the sentiment data to make this a viable approach.
    By starting with seed lists of words instead I was able to generate
    relatively small lists that I could review manually.

--- src/main/java/bio/terra/buffer/service/resource/projectid/GcpProjectIdGenerator.java
+++ src/main/java/bio/terra/buffer/service/resource/projectid/GcpProjectIdGenerator.java
@@ -3,6 +3,10 @@ package bio.terra.buffer.service.resource.projectid;
 import bio.terra.buffer.generated.model.ProjectIdSchema;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.hash.Hashing;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +18,9 @@ public class GcpProjectIdGenerator {
    * historical experience, increase if 8 is not enough for a pool's naming.
    */
   @VisibleForTesting static final int RANDOM_ID_SIZE = 8;
+
+  /** The largest number to use for random suffixes for adjective/noun project IDs. */
+  @VisibleForTesting static final int RANDOM_SUFFIX_LIMIT = 10000;
 
   /**
    * Generates project Id from prefix and scheme.
@@ -27,8 +34,11 @@ public class GcpProjectIdGenerator {
     }
 
     switch (projectIdSchema.getScheme()) {
+      case TWO_WORDS_NUMBER:
+        generatedId += WordPairs.getWithRandomSuffix();
+        break;
+
       case RANDOM_CHAR:
-      default:
         generatedId += generateRandomId();
         break;
     }
@@ -40,5 +50,24 @@ public class GcpProjectIdGenerator {
         .hashUnencodedChars(UUID.randomUUID().toString())
         .toString()
         .substring(0, RANDOM_ID_SIZE);
+  }
+
+  private static class WordPairs {
+    static final List<String> words = loadWords();
+    static final Random random = new Random();
+
+    static List<String> loadWords() {
+      List<String> words = new ArrayList<String>();
+      Scanner scanner =
+          new Scanner(WordPairs.class.getResourceAsStream("/random_project_prefixes.txt"));
+      while (scanner.hasNext()) {
+        words.add(scanner.next());
+      }
+      return words;
+    }
+
+    static String getWithRandomSuffix() {
+      return words.get(random.nextInt(words.size())) + "-" + random.nextInt(RANDOM_SUFFIX_LIMIT);
+    }
   }
 }
