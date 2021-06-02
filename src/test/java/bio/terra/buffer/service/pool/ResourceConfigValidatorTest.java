769cdf91eb1d35b812f6a70aea6a4ef6bc0567ea
Yonghao Yu <yyhao1@gmail.com>
2021-06-02 11:49:36 -0400

    [PF-806]remove compute api restriction (#157)
    * remove compute api
    
    * remove entire required apis

--- src/test/java/bio/terra/buffer/service/pool/ResourceConfigValidatorTest.java
+++ src/test/java/bio/terra/buffer/service/pool/ResourceConfigValidatorTest.java
@@ -12,9 +12,7 @@ import org.junit.jupiter.api.Test;
 public class ResourceConfigValidatorTest extends BaseUnitTest {
   private static GcpProjectConfig newValidGcpProjectConfig() {
     return new GcpProjectConfig()
-        .billingAccount("123")
-        .addEnabledApisItem("compute.googleapis.com")
-        .addEnabledApisItem("storage-component.googleapis.com");
+        .billingAccount("123");
   }
 
   @Test
@@ -35,19 +33,5 @@ public class ResourceConfigValidatorTest extends BaseUnitTest {
             InvalidPoolConfigException.class,
             () -> new GcpResourceConfigValidator().validate(resourceConfig));
     assertTrue(exception.getMessage().contains("Missing billing account"));
-  }
-
-  @Test
-  public void testValidateGcpConfig_missingRequiredApis() {
-    ResourceConfig resourceConfig =
-        new ResourceConfig()
-            .configName("testConfig")
-            .gcpProjectConfig(
-                newValidGcpProjectConfig().enabledApis(ImmutableList.of("foo.googleapis.com")));
-    InvalidPoolConfigException exception =
-        assertThrows(
-            InvalidPoolConfigException.class,
-            () -> new GcpResourceConfigValidator().validate(resourceConfig));
-    assertTrue(exception.getMessage().contains("Missing required enabledApis"));
   }
 }
