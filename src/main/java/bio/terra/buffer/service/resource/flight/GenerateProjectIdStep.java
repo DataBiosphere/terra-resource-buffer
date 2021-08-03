7cefa79a2e27b9d37f21153d3b7ce63ad29a7ccc
Mariko Medlock <mariko.medlock@gmail.com>
2021-08-03 17:28:43 -0400

    Use stairway retries for retrieveProject.

--- src/main/java/bio/terra/buffer/service/resource/flight/GenerateProjectIdStep.java
+++ src/main/java/bio/terra/buffer/service/resource/flight/GenerateProjectIdStep.java
@@ -2,6 +2,7 @@ package bio.terra.buffer.service.resource.flight;
 
 import static bio.terra.buffer.service.resource.FlightMapKeys.CLOUD_RESOURCE_UID;
 import static bio.terra.buffer.service.resource.FlightMapKeys.GOOGLE_PROJECT_ID;
+import static bio.terra.buffer.service.resource.flight.GoogleUtils.retrieveProject;
 
 import bio.terra.buffer.generated.model.CloudResourceUid;
 import bio.terra.buffer.generated.model.GcpProjectConfig;
@@ -35,15 +36,33 @@ public class GenerateProjectIdStep implements Step {
     FlightMap workingMap = flightContext.getWorkingMap();
     try {
       String projectId =
-          projectIdGenerator.generateIdWithRetries(gcpProjectConfig.getProjectIdSchema(), rmCow);
+          projectIdGenerator.generateIdWithRetries(gcpProjectConfig.getProjectIdSchema());
+
+      // check that a project with this id does not already exist. to do this check, we try
+      // to retrieve the project. this will only succeed if RBS has permission to get that
+      // project. we expect this to be the case most of the time because of the Terra-specific
+      // prefix, but it's possible this step will generate a project that is in use outside of
+      // Terra. in that case, the project creation step will fail.
+      if (retrieveProject(rmCow, projectId).isPresent()) {
+        logger.info("Generated GCP project is already in use: {}", projectId);
+
+        // here we use Stairway retry, instead of retrying immediately in order to rate limit
+        // the number of project get calls made per step execution. i.e. we could retry the
+        // project id generation immediately since we haven't hit a quota issue yet, but that
+        // would mean each execution of this step may make a variable number of project get
+        // calls. that would make it hard to decide how many flights could run in parallel
+        // without hitting project get quota issues.
+        return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY);
+      }
+
       flightContext.getWorkingMap().put(GOOGLE_PROJECT_ID, projectId);
       workingMap.put(
           CLOUD_RESOURCE_UID,
           new CloudResourceUid().googleProjectUid(new GoogleProjectUid().projectId(projectId)));
       return StepResult.getStepResultSuccess();
-    } catch (IOException | InterruptedException ioEx) {
-      logger.info("Error when generating GCP project id.", ioEx);
-      return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, ioEx);
+    } catch (IOException | InterruptedException ex) {
+      logger.info("Error when generating GCP project id.", ex);
+      return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, ex);
     }
   }
 
--- src/main/java/bio/terra/buffer/service/resource/projectid/GcpProjectIdGenerator.java
+++ src/main/java/bio/terra/buffer/service/resource/projectid/GcpProjectIdGenerator.java
@@ -1,26 +1,18 @@
 package bio.terra.buffer.service.resource.projectid;
 
-import static bio.terra.buffer.service.resource.flight.GoogleUtils.retrieveProject;
-
 import bio.terra.buffer.generated.model.ProjectIdSchema;
-import bio.terra.cloudres.google.cloudresourcemanager.CloudResourceManagerCow;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.hash.Hashing;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /** Generates Project Id for GCP project from config. */
 @Component
 public class GcpProjectIdGenerator {
-  private final Logger logger = LoggerFactory.getLogger(GcpProjectIdGenerator.class);
-
   /**
    * The size of project when generating random characters. Choose size as 8 based on AoU's
    * historical experience, increase if 8 is not enough for a pool's naming.
@@ -49,31 +41,18 @@ public class GcpProjectIdGenerator {
    * Generate a GCP project id from the prefix and scheme.
    *
    * <p>Keep retrying the id generation until we find one that fits into the GCP project id length
-   * limit and is not already in use, or until we have retried the maximum number of times.
-   *
-   * <p>To check if a project is in use, this method tries to retrieve the project. This will only
-   * succeed if RBS has permission to get that project. We expect this to be the case most of the
-   * time because of the Terra-specific prefix, but it's possible this step will generate a project
-   * that is in use outside of Terra. In that case, the project creation step will fail.
+   * limit, or until we have retried the maximum number of times.
    *
    * @param projectIdSchema prefix and naming scheme
-   * @param rmCow resource manager wrapper to retrieve the project
    * @return generated project id (prefix + naming scheme)
-   * @throws IOException if there is an error retrieving the project from GCP
    * @throws InterruptedException if no project id is found after the maximum number of retries
    */
-  public String generateIdWithRetries(
-      ProjectIdSchema projectIdSchema, CloudResourceManagerCow rmCow)
-      throws IOException, InterruptedException {
+  public String generateIdWithRetries(ProjectIdSchema projectIdSchema) throws InterruptedException {
     for (int numTries = 0; numTries < MAX_RETRIES; numTries++) {
       String projectId = generateId(projectIdSchema);
-      if (projectId.length() <= MAX_LENGTH_GCP_PROJECT_ID)
-        if (retrieveProject(rmCow, projectId).isEmpty()) {
-          logger.info("Generated GCP project id after {} tries.", numTries);
-          return projectId;
-        } else {
-          logger.info("Generated GCP project is already in use: {}", projectId);
-        }
+      if (projectId.length() <= MAX_LENGTH_GCP_PROJECT_ID) {
+        return projectId;
+      }
     }
     throw new InterruptedException(
         "No project id found after maximum number of retries: " + MAX_RETRIES);
