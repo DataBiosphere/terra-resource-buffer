95d866f896b43d7b9f8c3f344b1cb2bef09229e3
Chelsea Hoover <chelsea@broadinstitute.org>
2021-08-12 13:17:09 -0400

    DDO-1319 Update local dev workflow for buffer to use render

--- local-dev/setup_gke_deploy.sh
+++ local-dev/setup_gke_deploy.sh
@@ -9,19 +9,33 @@ set -e
 # Required input
 ENV=$1
 # Optional input
-TERRA_HELM_BRANCH=${2:-master}
-TERRA_HELMFILE_BRANCH=${3:-master}
+TERRA_HELMFILE_BRANCH=${2:-master}
+GIT_PROTOCOL=${3:-http}
+
+if [ "$GIT_PROTOCOL" = "http" ]; then
+    helmfilegit=https://github.com/broadinstitute/terra-helmfile
+else
+    helmfilegit=git@github.com:broadinstitute/terra-helmfile.git
+fi
 
 # Clone Helm chart and helmfile repos
-rm -rf terra-helm
 rm -rf terra-helmfile
-git clone -b "$TERRA_HELM_BRANCH" --single-branch https://github.com/broadinstitute/terra-helm.git
-git clone -b "$TERRA_HELMFILE_BRANCH" --single-branch https://github.com/broadinstitute/terra-helmfile.git
+git clone -b "$TERRA_HELMFILE_BRANCH" --single-branch "${helmfilegit}"
 
 # Template in environment
 sed "s|ENV|${ENV}|g" skaffold.yaml.template > skaffold.yaml
 sed "s|ENV|${ENV}|g" values.yaml.template > values.yaml
 
+# Render manifests to terra-helmfile/output/ directory.
+#
+# Unfortunately we need to render them all into a single mega-file
+# because Skaffold's `kubectl` deployment does not support
+# recursive globbing like "output/manifests/**/*.yaml"
+mkdir -p ./terra-helmfile/output
+./terra-helmfile/bin/render \
+  -e "${ENV}" \
+  -a buffer \
+  --stdout > terra-helmfile/output/manifests.yaml
 
 # That's it! You can now deploy to the k8s cluster by running
 # $ skaffold run
