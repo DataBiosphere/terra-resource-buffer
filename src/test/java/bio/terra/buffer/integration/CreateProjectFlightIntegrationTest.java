ba19c275eb66e1ae13408ea8bbc30f8c3e9c6c22
yonghao <yyhao1@gmail.com>
2021-11-03 22:08:11 -0400

    disable defualt network test

--- src/test/java/bio/terra/buffer/integration/CreateProjectFlightIntegrationTest.java
+++ src/test/java/bio/terra/buffer/integration/CreateProjectFlightIntegrationTest.java
@@ -101,6 +101,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -208,6 +209,8 @@ public class CreateProjectFlightIntegrationTest extends BaseIntegrationTest {
     assertDefaultServiceAccountExists(project);
   }
 
+  @Disabled(
+      "In Broad deployment, skipDefaultNetworkCreation is turned on orignization policy")
   @Test
   public void testCreateGoogleProject_keepDefaultNetwork() throws Exception {
     FlightManager manager =
