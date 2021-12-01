67d2ffc7840e016ad174d3921756366387f2cb84
Jack Warren <jack@jackwarren.info>
2021-11-30 19:38:12 -0500

    remote html tags

--- src/main/java/bio/terra/buffer/app/configuration/PrometheusConfiguration.java
+++ src/main/java/bio/terra/buffer/app/configuration/PrometheusConfiguration.java
@@ -13,23 +13,20 @@ public class PrometheusConfiguration {
     /**
      * Wires in OpenCensus's stats underneath the Prometheus registry that
      * Micrometer exposes via Spring Boot's "Actuator" pattern.
-     * <p>
+     *
      * Spring Boot has built-in Prometheus support--it will handle
      * initialization and the endpoint automatically--but exclusively in
      * concert with the Micrometer library. This app is already instrumented
      * with OpenCensus, though, and we don't want to re-instrument with
      * Micrometer instead.
-     * </p>
-     * <p>
+     *
      * The solution: OpenCensus will happily work directly with a raw
      * Prometheus {@link io.prometheus.client.CollectorRegistry}, so we have
      * it register with the underlying instance that Micrometer and Spring Boot
      * are sharing to power the automatic endpoint.
-     * </p>
-     * <p>
+     *
      * @see PrometheusStatsCollector#createAndRegister(PrometheusStatsConfiguration)
      * @see PrometheusMeterRegistry#getPrometheusRegistry()
-     * </p>
      */
     @Bean
     public MeterRegistryCustomizer<PrometheusMeterRegistry> registerOpenCensus() {
