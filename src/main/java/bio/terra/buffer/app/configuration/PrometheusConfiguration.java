dc31b067721ea0a4ce720e52a0a58f2632ca0090
Jack Warren <jack@jackwarren.info>
2021-12-01 12:42:15 -0500

    run Prometheus on 8081

--- src/main/java/bio/terra/buffer/app/configuration/PrometheusConfiguration.java
+++ src/main/java/bio/terra/buffer/app/configuration/PrometheusConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "buffer.prometheus")
 public class PrometheusConfiguration {
-  /** Whether to enable metrics scraping by prometheus. */
+  /** Whether to enable a metrics endpoint for prometheus scraping */
   private boolean enabled = true;
+  /** The port to run the metrics endpoint on */
+  private int port = 8081;
 
   public boolean isEnabled() {
     return enabled;
@@ -17,5 +19,13 @@ public class PrometheusConfiguration {
 
   public void setEnabled(boolean enabled) {
     this.enabled = enabled;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
   }
 }
---
+++ src/main/java/bio/terra/buffer/service/prometheus/PrometheusEndpoint.java
@@ -0,0 +1,60 @@
+package bio.terra.buffer.service.prometheus;
+
+import bio.terra.buffer.app.configuration.PrometheusConfiguration;
+import io.opencensus.exporter.stats.prometheus.PrometheusStatsCollector;
+import io.prometheus.client.exporter.HTTPServer;
+
+import java.io.IOException;
+import java.util.Objects;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * A component that automatically runs a Prometheus endpoint server on a different port,
+ * exporting stats from OpenCensus.
+ */
+@Component
+public class PrometheusEndpoint {
+    private final Logger logger = LoggerFactory.getLogger(PrometheusEndpoint.class);
+    private final PrometheusConfiguration prometheusConfiguration;
+
+    private HTTPServer prometheusServer;
+
+    @Autowired
+    public PrometheusEndpoint(PrometheusConfiguration prometheusConfiguration) {
+        this.prometheusConfiguration = prometheusConfiguration;
+    }
+
+    @PostConstruct
+    private void startEndpointServer() {
+        logger.info("Prometheus enabled: {}.", prometheusConfiguration.isEnabled());
+        if (!prometheusConfiguration.isEnabled()) {
+            return;
+        }
+        try {
+            PrometheusStatsCollector.createAndRegister();
+        } catch (IllegalArgumentException e) {
+            logger.error("OpenCensus Prometheus Collector already registered.", e);
+        }
+        try {
+            prometheusServer = new HTTPServer(prometheusConfiguration.getPort());
+            logger.info("Prometheus server started: port {}", prometheusConfiguration.getPort());
+        } catch (IOException e) {
+            logger.error("Prometheus server error on startup.", e);
+        }
+    }
+
+    @PreDestroy
+    private void stopEndpointServer() {
+        if (Objects.isNull(prometheusServer)) {
+            return;
+        }
+        prometheusServer.close();
+        logger.info("Prometheus server stopped");
+    }
+}
