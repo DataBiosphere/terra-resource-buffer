0073e74c9d314ada9cc5e2ea78fa2c4e8f874305
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2022-03-10 16:23:36 -0500

    PR notes

--- src/main/java/bio/terra/buffer/service/resource/flight/GoogleProjectConfigUtils.java
+++ src/main/java/bio/terra/buffer/service/resource/flight/GoogleProjectConfigUtils.java
@@ -84,14 +84,16 @@ public class GoogleProjectConfigUtils {
   }
 
   /**
-   * Create a Consumer Quota Override for BigQuery Daily Query Usage If the configuration for
-   * isOverrideBigQueryDailyUsageQuota is true, return an Optional of the value in
-   * bigQueryDailyUsageQuotaOverrideValueBytes. Otherwise, return empty.
+   * Create a Consumer Quota Override for BigQuery Daily Query Usage.
+   * If the configuration for isOverrideBigQueryDailyUsageQuota is true and
+   * bigQueryDailyUsageQuotaOverrideValueBytes is non-null, return an Optional
+   * of the value in bigQueryDailyUsageQuotaOverrideValueBytes. Otherwise, return empty.
    */
   public static Optional<Long> bigQueryDailyUsageOverrideValueBytes(
       GcpProjectConfig gcpProjectConfig) {
     BigQueryQuotas bigQueryQuotas = gcpProjectConfig.getServiceUsage().getBigQuery();
-    if (!bigQueryQuotas.isOverrideBigQueryDailyUsageQuota()) {
+    if (!bigQueryQuotas.isOverrideBigQueryDailyUsageQuota()
+        || null == bigQueryQuotas.getBigQueryDailyUsageQuotaOverrideValueBytes()) {
       return Optional.empty();
     }
     long value = bigQueryQuotas.getBigQueryDailyUsageQuotaOverrideValueBytes().longValue();
