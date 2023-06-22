8290a9dd7da24d4ddb56c7af835f4970f6dd8b94
zloery <6668057+zloery@users.noreply.github.com>
2023-06-22 11:39:32 -0400

    add explicit validation for RANDOM_CHAR pattern

--- src/main/java/bio/terra/buffer/service/pool/GcpResourceConfigValidator.java
+++ src/main/java/bio/terra/buffer/service/pool/GcpResourceConfigValidator.java
@@ -1,10 +1,13 @@
 package bio.terra.buffer.service.pool;
 
+import static bio.terra.buffer.service.resource.projectid.GcpProjectIdGenerator.MAX_LENGTH_GCP_PROJECT_ID;
 import static bio.terra.buffer.service.resource.projectid.GcpProjectIdGenerator.MAX_LENGTH_GCP_PROJECT_ID_PREFIX;
+import static bio.terra.buffer.service.resource.projectid.GcpProjectIdGenerator.RANDOM_ID_SIZE;
 
 import bio.terra.buffer.common.exception.InvalidPoolConfigException;
 import bio.terra.buffer.generated.model.GcpProjectConfig;
 import bio.terra.buffer.generated.model.ProjectIdSchema;
+import bio.terra.buffer.generated.model.ProjectIdSchema.SchemeEnum;
 import bio.terra.buffer.generated.model.ResourceConfig;
 
 /**
@@ -33,6 +36,13 @@ public class GcpResourceConfigValidator implements ResourceConfigValidator {
       throw new InvalidPoolConfigException(
           String.format(
               "Project id prefix is too long for TWO_WORDS_NUMBER naming scheme: %s",
+              config.getConfigName()));
+    } else if (gcpProjectConfig.getProjectIdSchema().getScheme().equals(SchemeEnum.RANDOM_CHAR)
+        && gcpProjectConfig.getProjectIdSchema().getPrefix().length() + RANDOM_ID_SIZE
+            > MAX_LENGTH_GCP_PROJECT_ID) {
+      throw new InvalidPoolConfigException(
+          String.format(
+              "Project id prefix is too long for RANDOM_CHAR naming scheme: %s",
               config.getConfigName()));
     }
   }
