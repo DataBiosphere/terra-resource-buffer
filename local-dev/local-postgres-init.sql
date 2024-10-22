4f971c8a0853d2f5dcb77751b1230d92e2ba1dda
David An <davidan@broadinstitute.org>
2024-10-22 13:22:33 -0400

    no quotes

--- local-dev/local-postgres-init.sql
+++ local-dev/local-postgres-init.sql
@@ -3,5 +3,5 @@ CREATE DATABASE testdb_stairway;
 CREATE ROLE dbuser WITH LOGIN ENCRYPTED PASSWORD 'dbpwd';
 CREATE ROLE dbuser_stairway WITH LOGIN ENCRYPTED PASSWORD 'dbpwd_stairway';
 
-ALTER DATABASE testdb OWNER TO 'dbuser';
-ALTER DATABASE testdb_stairway OWNER TO 'dbuser_stairway';
+ALTER DATABASE testdb OWNER TO dbuser;
+ALTER DATABASE testdb_stairway OWNER TO dbuser_stairway;
