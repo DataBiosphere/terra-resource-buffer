17f0b2061cf70276f704d9e26738b701f21ec512
David An <davidan@broadinstitute.org>
2024-10-22 15:18:57 -0400

    create/usage permissions, not owner

--- local-dev/local-postgres-init.sql
+++ local-dev/local-postgres-init.sql
@@ -3,5 +3,8 @@ CREATE DATABASE testdb_stairway;
 CREATE ROLE dbuser WITH LOGIN ENCRYPTED PASSWORD 'dbpwd';
 CREATE ROLE dbuser_stairway WITH LOGIN ENCRYPTED PASSWORD 'dbpwd_stairway';
 
-ALTER DATABASE testdb OWNER TO dbuser;
-ALTER DATABASE testdb_stairway OWNER TO dbuser_stairway;
+GRANT USAGE ON SCHEMA testdb.public TO dbuser;
+GRANT CREATE ON SCHEMA testdb.public TO dbuser;
+
+GRANT USAGE ON SCHEMA testdb_stairway.public TO dbuser_stairway;
+GRANT CREATE ON SCHEMA testdb_stairway.public TO dbuser_stairway;
