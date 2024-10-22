3c2f8b84f64442b4625efa31ab33924dc1e6a5ec
David An <davidan@broadinstitute.org>
2024-10-22 15:35:23 -0400

    back to owner

--- local-dev/local-postgres-init.sql
+++ local-dev/local-postgres-init.sql
@@ -1,10 +1,5 @@
-CREATE DATABASE testdb;
-CREATE DATABASE testdb_stairway;
 CREATE ROLE dbuser WITH LOGIN ENCRYPTED PASSWORD 'dbpwd';
 CREATE ROLE dbuser_stairway WITH LOGIN ENCRYPTED PASSWORD 'dbpwd_stairway';
 
-GRANT USAGE ON SCHEMA "testdb.public" TO dbuser;
-GRANT CREATE ON SCHEMA "testdb.public" TO dbuser;
-
-GRANT USAGE ON SCHEMA "testdb_stairway.public" TO dbuser_stairway;
-GRANT CREATE ON SCHEMA "testdb_stairway.public" TO dbuser_stairway;
+CREATE DATABASE testdb OWNER dbuser;
+CREATE DATABASE testdb_stairway OWNER dbuser_stairway;
