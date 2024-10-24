449df06e14a284902d02c3d8f11278fd8ecf34cc
David An <davidan@broadinstitute.org>
2024-10-24 16:42:07 -0400

    CORE-3: upgrade to Postgres 16.3 for unit tests (#365)

--- local-dev/local-postgres-init.sql
+++ local-dev/local-postgres-init.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE testdb;
-CREATE DATABASE testdb_stairway;
 CREATE ROLE dbuser WITH LOGIN ENCRYPTED PASSWORD 'dbpwd';
 CREATE ROLE dbuser_stairway WITH LOGIN ENCRYPTED PASSWORD 'dbpwd_stairway';
+
+CREATE DATABASE testdb OWNER dbuser;
+CREATE DATABASE testdb_stairway OWNER dbuser_stairway;
