091178b1953238b24afc652b3854acb567101b3f
Bria Morgan <bmorgan@broadinstitute.org>
2025-07-14 15:14:01 -0400

    first pass

--- terra-resource-buffer-client/publish.sh
+++ terra-resource-buffer-client/publish.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-# Copied from Terra Janitor repo
-# Publish Terra Resource Buffer Service Client Package:
-VAULT_TOKEN=${1:-$(cat $HOME/.vault-token)}
-DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:consul-0.20.0
-ARTIFACTORY_ACCOUNT_PATH=secret/dsp/accts/artifactory/dsdejenkins
-
-export ARTIFACTORY_USERNAME=$(docker run -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOLBOX_DOCKER_IMAGE} \
- vault read -field username ${ARTIFACTORY_ACCOUNT_PATH})
-export ARTIFACTORY_PASSWORD=$(docker run -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOLBOX_DOCKER_IMAGE} \
- vault read -field password ${ARTIFACTORY_ACCOUNT_PATH})
+export GOOGLE_CLOUD_PROJECT=dsp-artifact-registry
+export GAR_LOCATION=us-central1
+export GAR_REPOSITORY_ID=libs-snapshot-standard
 ./gradlew test
-./gradlew artifactoryPublish
+./gradlew publish
