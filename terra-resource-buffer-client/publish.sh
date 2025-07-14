903857dcd6400532032a12a2952826f31f18052b
Bria Morgan <bmorgan@broadinstitute.org>
2025-07-14 15:52:42 -0400

    update publish.sh and readme

--- terra-resource-buffer-client/publish.sh
+++ terra-resource-buffer-client/publish.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+# Default to snapshot repository
+REPO_TYPE=${1:-snapshot}
+
 export GOOGLE_CLOUD_PROJECT=dsp-artifact-registry
 export GAR_LOCATION=us-central1
-export GAR_REPOSITORY_ID=libs-snapshot-standard
+
+if [ "$REPO_TYPE" = "release" ]; then
+    export GAR_REPOSITORY_ID=libs-release-standard
+    echo "Publishing to release repository: $GAR_REPOSITORY_ID"
+else
+    export GAR_REPOSITORY_ID=libs-snapshot-standard
+    echo "Publishing to snapshot repository: $GAR_REPOSITORY_ID"
+fi
+
 ./gradlew test
 ./gradlew publish
