60654857c09554184e66b996661359f19fdb47b7
David An <davidan@broadinstitute.org>
2025-09-24 16:04:12 -0400

    remove deprecated uses of @MockBean

--- src/test/java/bio/terra/buffer/app/controller/BufferApiControllerTest.java
+++ src/test/java/bio/terra/buffer/app/controller/BufferApiControllerTest.java
@@ -33,10 +33,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -50,7 +50,8 @@ public class BufferApiControllerTest {
   @Autowired private MockMvc mvc;
   @Autowired private BufferDao bufferDao;
   @Autowired private ObjectMapper objectMapper;
-  @MockBean private MetricsHelper mockMetricsHelper;
+  @MockitoBean
+  private MetricsHelper mockMetricsHelper;
 
   @Test
   public void handoutResource_ok() throws Exception {
